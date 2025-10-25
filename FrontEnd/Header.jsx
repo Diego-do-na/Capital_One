05633eb9478d97152c0cb1c1a51e7dd2a92a2882
Soy3miliano <youpollos@gmail.com>
2025-10-25 16:38:09 -0600

    Create Header.jsx

---
+++ FrontEnd/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Usamos 'export function' porque lo importas con { }
+export function Header({ onMenuClick, isMirrorsOn, onToggleChange }) {
+  return (
+    <header className="app-header">
+      <button onClick={onMenuClick}>☰</button>
+      <h2>Mirrors</h2>
+      <label>
+        Activo:
+        <input
+          type="checkbox"
+          checked={isMirrorsOn}
+          onChange={onToggleChange}
+        />
+      </label>
+    </header>
+  );
+}
